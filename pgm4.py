da795afa160b20119ca7415fed03a0a667719740
vishnu123266 <vishnubinish4@gmail.com>
2025-09-11 15:43:13 +0530

    Add files via upload

---
+++ pgm4.py
@@ -0,0 +1,27 @@
+num_int =25
+print("Integer:", num_int,"| Type:", type(num_int))
+
+num_float =12.75
+print("Float:", num_float,"| Type:", type(num_float))
+
+text="Hello,pyhton!"
+print("String", text,"| Type:", type(text))
+
+is_python_easy =True
+print("Boolean",is_python_easy ,"| Type:", type(is_python_easy))
+
+fruits =["apple","banana","cherry"]
+print("List:",fruits,"| Type:", type(fruits))
+
+coordinates =(10,20,30)
+print("Tuple",coordinates,"| Type:", type(coordinates))
+
+unique_numbers = {1,2,3,3,2}
+print("Set",unique_numbers,"| Type:", type(unique_numbers))
+
+student={"name":"Alice","age":21,"course":"Computer science"}
+print("Dictionary:",student,"| Type:", type(student))
+
+num_complex =3+4j
+print("Complex Number:",num_complex,"|Type:", type(num_complex))
+
